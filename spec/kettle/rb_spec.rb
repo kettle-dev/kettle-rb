33c5c5317398c2cad62c47528cb89004285292f9
Peter H. Boling <peter.boling@gmail.com>
2026-06-27 10:19:12 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- spec/kettle/rb_spec.rb
+++ spec/kettle/rb_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Kettle::Rb do
   it "has a version number" do
-    expect(Kettle::Rb::VERSION).not_to be nil
+    expect(Kettle::Rb::VERSION).not_to be_nil
   end
 
   it "does something useful" do
