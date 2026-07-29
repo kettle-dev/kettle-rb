b063f9e867c03772f3fff25ecf6dc382a267d90e
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 16:34:31 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

---
+++ spec/kettle/rb/version_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "anonymous_loader"
+RSpec.describe Kettle::Rb::Version do
+  it_behaves_like "a Version module", described_class
+
+  it "executes the version file for coverage without redefining constants" do
+    path = File.expand_path("../../../lib/kettle/rb/version.rb", __dir__)
+    anonymous_namespace = AnonymousLoader.load(:files => path)
+
+    expect(anonymous_namespace::Kettle::Rb::Version::VERSION).to eq(described_class::VERSION)
+  end
+end
