655e67ed31024dc0f5e9a53490d93c5b32ee8ec2
Peter H. Boling <peter.boling@gmail.com>
2026-07-30 20:59:30 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/kettle/rb/version_spec.rb
+++ spec/kettle/rb/version_spec.rb
@@ -2,14 +2,6 @@
 
 require "anonymous_loader"
 RSpec.describe Kettle::Rb::Version do
-  it "defines the gem version" do
-    expect(described_class::VERSION).to match(/\A\d+\.\d+\.\d+/)
-  end
-
-  it "exposes the version at the traditional constant location" do
-    expect(Kettle::Rb::VERSION).to eq(described_class::VERSION)
-  end
-
   it "executes the version file for coverage without redefining constants" do
     path = File.expand_path("../../../lib/kettle/rb/version.rb", __dir__)
     anonymous_namespace = AnonymousLoader.load(:files => path)
